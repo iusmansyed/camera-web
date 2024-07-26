6c0f783ed6a660f8294ee5ba723873796e117dde
syed muhammad usman <syedmuhammadusman015@gmail.com>
2024-07-26 15:46:28 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from '@vite-pwa/vite-plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // Enables PWA in development mode
+      },
+      manifest: {
+        name: 'My PWA App',
+        short_name: 'PWA App',
+        description: 'My Progressive Web App built with Vite and React',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'assets/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'assets/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
